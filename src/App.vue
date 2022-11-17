a928cc589fcb4b2bf9af98fe0068118f03fd2f73
Mirko Robbi <mirko.robbi@gmail.com>
2022-11-17 13:02:30 +0100

    add components and set variables

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
 
 export default {
   components: {
+    AppHeader,
+    AppMain,
+    AppFooter
   },
   data() {
     return {
@@ -15,6 +21,9 @@ export default {
 
 <template>
 
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 
 </template>
 

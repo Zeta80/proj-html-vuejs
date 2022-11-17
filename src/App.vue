5390127261e80da0e609257f1a8310881f7bd104
Mirko Robbi <mirko.robbi@gmail.com>
2022-11-17 12:32:40 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+@use "./styles/partials/variables" as *;
+</style>

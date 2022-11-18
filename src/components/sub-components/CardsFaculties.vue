36498909098ea7430d47a13e6da2d7f9a3c4bdf9
Mirko Robbi <mirko.robbi@gmail.com>
2022-11-18 13:37:47 +0100

    add active element

--- src/components/sub-components/CardsFaculties.vue
+++ src/components/sub-components/CardsFaculties.vue
@@ -22,8 +22,9 @@ export default {
     <div class="wrapper-border">
         <div class="container">
             <div class="wrapper-card">
-                <div class="row">
-                    <div v-for="(fac, index ) in store.faculties" :key="index" class="col">
+                <div class="row row-cols-sd-2 row-cols-md-5">
+                    <div :class="(fac.active == true ? `active` : `no-active`)" v-for="(fac, index ) in store.faculties"
+                        :key="index" class="col my-card">
                         <img :src="getImgUrl(index)" alt="">
                         <p> {{ fac.title }} </p>
                     </div>
@@ -40,14 +41,40 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@use "../../styles/partials/variables" as *;
+
 .wrapper-border {
     width: 100%;
-    border: 1px solid green;
+    border: 1px solid $border-color-grey;
 
     .wrapper-card {
         width: 90%;
         margin: 0 auto;
         text-align: center;
+
+        .my-card {
+            border-left: 1px solid $border-color-grey;
+            cursor: pointer;
+
+            &:last-child {
+                border-right: 1px solid $border-color-grey;
+            }
+
+            &.active {
+                background-color: $fill-color-red;
+                color: $text-color-mwhite;
+
+                img {
+                    filter: brightness(8);
+                }
+            }
+
+
+            img {
+                min-width: 90px;
+                max-width: 90px;
+            }
+        }
     }
 }
 </style>
